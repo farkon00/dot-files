d73fe8a53c1fc6550866002041e6a794a1585561
farkon00 <59134591+farkon00@users.noreply.github.com>
2024-07-24 14:04:44 +0300

    Rostyk nvim changes
    * Changed tab size to 4 in the init.lua file
    * Added telescope and its key bindings
    * Added a few basic key bindings
    * Changed line numbering

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd[[au VimEnter * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escap
 vim.cmd[[au VimLeave * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock']]
 
 vim.opt.termguicolors = true
+vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -11,43 +12,50 @@ vim.opt.autoindent = true
 vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.backspace = "indent,eol,start"
+vim.g.mapleader = " "
 vim.opt.clipboard:append("unnamedplus")
 
 vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_dd')
 
 -- Installing the plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-  },
-  {
-    "nvim-tree/nvim-web-devicons",
-    lazy = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    lazy = false,
-  } 
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        lazy = false,
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+        lazy = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        lazy = false,
+    },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    }            
 })
 
 -- Appearance
@@ -55,35 +63,35 @@ vim.cmd[[colorscheme tokyonight]]
 
 -- File explorer
 require("nvim-tree").setup({
-  view = {
-    width = 30,
-    signcolumn = "yes",
-  },
-  renderer = {
-    group_empty = true,
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "├",
-        bottom = "─",
-        none = " ",
-      },
+    view = {
+        width = 30,
+        signcolumn = "yes",
     },
-    icons = {
-      git_placement = "signcolumn",
-      show = {
-        folder_arrow = false
-      }
-    }
-  },
-  filters = {
-    dotfiles = false,
-  },
+    renderer = {
+        group_empty = true,
+        indent_markers = {
+            enable = true,
+            icons = {
+                corner = "└",
+                edge = "│",
+                item = "├",
+                bottom = "─",
+                none = " ",
+            },
+        },
+        icons = {
+            git_placement = "signcolumn",
+            show = {
+                folder_arrow = false
+            }
+        }
+    },
+    filters = {
+        dotfiles = false,
+    },
 })
 vim.api.nvim_create_user_command("Exp", function()
-    vim.cmd[[:NvimTreeToggle]]
+        vim.cmd[[:NvimTreeToggle]]
 end, {})
 
 -- Git diff
@@ -92,21 +100,22 @@ require('gitsigns').setup()
 -- LSP
 local lspconfig = require("lspconfig")
 lspconfig.pyright.setup {
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "off"
-      }
+    settings = {
+        python = {
+            analysis = {
+                typeCheckingMode = "off"
+            }
+        }
     }
-  }
 }
 lspconfig.tsserver.setup {}
+lspconfig.clangd.setup {}
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-  end
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    end
 })
 vim.cmd[[:set completeopt-=preview]]
 
@@ -114,3 +123,8 @@ vim.keymap.set("i", "<c-z>", "<c-x><c-o>", { silent = true })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { silent = true })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
